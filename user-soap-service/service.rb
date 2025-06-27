2288f2c0cd4a4d4de9fa64b08c639a836a7e1fb5
Bryan03A <hacker03291997@gmail.com>
2025-06-27 07:52:44 -0500

    Test

--- user-soap-service/service.rb
+++ user-soap-service/service.rb
@@ -10,19 +10,6 @@ configure do
   enable :cross_origin
 end
 
-before do
-  allowed_origin = 'http://3.227.120.143:8080' # interfaz de usuario
-  origin = request.env['HTTP_ORIGIN']
-
-  if origin == allowed_origin
-    response.headers['Access-Control-Allow-Origin'] = origin
-    response.headers['Vary'] = 'Origin'
-  end
-
-  response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-  response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-end
-
 options '*' do
   allowed_origin = 'http://3.227.120.143:8080'
   origin = request.env['HTTP_ORIGIN']
